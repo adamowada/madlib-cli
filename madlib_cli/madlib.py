f1e471d45ce78dda822b01dae92c9baebb289db2
Adam Owada <adamowada@gmail.com>
2020-06-16 23:14:37 -0700

    updated readme

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,10 +1,3 @@
-# https://stackoverflow.com/questions/47782353/python-using-regex-to-search-for-a-string-then-append-each-match-to-a-new-list helped me with regex
-# https://www.geeksforgeeks.org/python-regex-re-search-vs-re-findall/ helped me with regex findall
-# https://stackoverflow.com/questions/413071/regex-to-get-string-between-curly-braces?newreg=c69f3c6ab1a34d9889687842884323b7 helped me with the regular expression string itself
-# https://docs.python.org/2/library/re.html helped me a lot with regex methods
-# https://stackoverflow.com/questions/59313087/python-regex-how-to-replace-each-match-individually helped me with the idea to use match objects
-# https://docs.python.org/3/library/re.html#match-objects helped me with match objects
-
 import re
 
 print("""

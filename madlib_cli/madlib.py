dd7428687ed7bb7b9a16dd62c5ee64772855232b
Adam Owada <adamowada@gmail.com>
2020-06-16 23:54:08 -0700

    test

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,39 +1,37 @@
 import re
 
-print("""
-Welcome to the Madlib cli app!
+def madlib():
+    print("""
+    Welcome to the Madlib cli app!
 
-Prompts will ask you to input
-certain types of words. For example,
-if the prompt asks for a color you
-might type "red". 
+    Prompts will ask you to input
+    certain types of words. For example,
+    if the prompt asks for a color you
+    might type "red". 
 
-After you type your response press
-enter to continue.
+    After you type your response press
+    enter to continue.
 
-After all prompts have been entered, 
-the finished madlib will be displayed 
-and saved as a text file. Have fun!
+    After all prompts have been entered, 
+    the finished madlib will be displayed 
+    and saved as a text file. Have fun!
 
-""")
+    """)
+    template = ""
+    with open('sample_template.txt', 'r') as template:
+        template = template.read()
+    regex1 = '{([^}]*)}'
+    regex2 = '({[^}]*})'
+    matches = re.findall(regex1, template)
+    for i in range(len(matches)):
+        m = re.search(regex2, template)
+        print(matches[i])
+        template = template[:m.start()] + input() + template[m.end():]
+        print('\n')
+    print(template)
+    with open('madlib_result.txt', 'w+') as result:
+        result.write(template)
+    return template
 
-template = ""
-
-with open('sample_template.txt', 'r') as template:
-    template = template.read()
-
-regex1 = '{([^}]*)}'
-regex2 = '({[^}]*})'
-
-matches = re.findall(regex1, template)
-
-for i in range(len(matches)):
-    m = re.search(regex2, template)
-    print(matches[i])
-    template = template[:m.start()] + input() + template[m.end():]
-    print('\n')
-
-print(template)
-
-with open('madlib_result.txt', 'w+') as result:
-    result.write(template)
+if __name__ == '__main__':
+    madlib()
